bff621f22816dadbb5a9501ed644571de08d9470
Zeal <40761189+ZealKinyanjui@users.noreply.github.com>
2018-07-18 16:44:00 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,22 @@
 // Select color input
+const chooseColor = ;
+
+
 // Select size input
+let gridWidth, gridHeight, color;
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
-
+  for (var row = 1; row<=gridWidth; row++) {
+    document.
+    for (var col = 1; col<=gridLength; col++) {
+      document.
+  }
 }
+
+  do (document.clear(*function*) {
+    while ( = ) 
+      ++;}
+
